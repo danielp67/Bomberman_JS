4cb8eda9f5dfd0f1baecbc959e2e9141b3faaf95
danielp67 <daniel.67100@hotmail.fr>
2020-06-29 19:38:35 +0200

    sauvday1 18h

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
 
 
 // Déplacement du joueur
-const joueur = document.querySelector(".joueur");
-const plateau = document.querySelector(".plateau_jeu");
+const player = document.querySelector(".player");
+const gameboard = document.querySelector(".gameboard");
 
 
 window.addEventListener("keydown", function(event) {
-	let touche=event.code;
-	let x = joueur.offsetLeft;
-	let y = joueur.offsetTop;
+	let touch=event.code;
+	let x = player.offsetLeft;
+	let y = player.offsetTop;
 
-	switch (touche) {
+	switch (touch) {
 		
 		case 'Space':	
 			alert("espace");
@@ -35,12 +35,12 @@ window.addEventListener("keydown", function(event) {
 		default:
 
 	}
-if(x>="0" && x<"500"){
-	joueur.style.left= String(x)+'px';
+if(x>=0 && x<500){
+	player.style.left= String(x)+'px';
 }
 
-if(y>="0" && y<"500"){
-	joueur.style.top= String(y)+'px';
+if(y>=0 && y<500){
+	player.style.top= String(y)+'px';
 
 }
 
@@ -49,31 +49,69 @@ if(y>="0" && y<"500"){
 
 //Création des ennemis
 
-let ennemisx = [1,2,3,4,5,6,7,8,9,10];
-let ennemisy = [1,2,3,4,5,6,7,8,9,10];
+let nbEnemy=5;
 
-for(let i=0; i>ennemisx.length; i++){
-	let min=0;
-	let max=10;
-	ennemisx[i]=Math.floor(Math.random()*(max-min))*50;
+
+for(let i=0; i<nbEnemy; i++){
+	let x=Math.floor(Math.random()*10)*50;
+	let y=Math.floor(Math.random()*10)*50;
+	let enemy=document.createElement("div");
+	enemy.classList.add("enemy");
+	gameboard.appendChild(enemy);
+	enemy.style.left= String(x)+'px';
+	enemy.style.top= String(y)+'px'
+}
+
+
+
+
+
+	//Déplacement des ennemis
+
+function updatePosition(){
+	let enemys=gameboard.querySelectorAll(".enemy");
+
+	for(let i=0; i<enemys.length; i++){
+		let x = enemys[i].offsetLeft;
+		let y = enemys[i].offsetTop;
+		let position=Math.floor(Math.random()*4);
+
+		switch (position) {
+
+			case 0:
+				x-=50;		
+				break;
+			
+			case 1:
+				y-=50;
+				break;
+			
+			case 2:
+				x+=50;
+				break;
+			
+			case 3:
+				y+=50;
+				break;
+			
+			default:
 	
-}
-
-
-for(let i=0; i>ennemisy.length; i++){
-	let min=0;
-	let max=10;
-	ennemisy[i]=Math.floor(Math.random()*(max-min))*50;
+		}
+	if(x>=0 && x<500){
+		enemys[i].style.left= String(x)+'px';
+	}
 	
+	if(y>=0 && y<500){
+		enemys[i].style.top= String(y)+'px';
+	
+	}		
+	}
+
 }
 
 
-for(let i=0; i>ennemisx.length; i++){
-	let ennemi=document.createElement("div");
-	ennemi.classList.add(ennemi);
-	plateau.appendChild(ennemi);
-	let x=ennemisx[i];
-	let y=ennemisy[i];
-	ennemi.style.left= String(x)+'px';
-	ennemi.style.top= String(y)+'px'
-}
+	let interval=setInterval(updatePosition,1000); 
+
+
+
+
